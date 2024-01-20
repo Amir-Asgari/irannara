49ddf95f43317f3e708197e3c01bfe0c3b7c21f3
Amir Asgari <Mr.a.asgari@gmail.com>
2024-01-21 00:06:04 +0330

    clean the git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,19 @@
+import {MatAccordion, MatExpansionModule} from '@angular/material/expansion';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule, MatDivider, MatDividerModule, MatIconModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule, Routes }   from '@angular/router';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
   declarations: [
@@ -13,7 +22,21 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    FormsModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    RouterModule,
+    MatTooltipModule,
+    MatExpansionModule,
+    MatMenuModule,
+    RouterModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
